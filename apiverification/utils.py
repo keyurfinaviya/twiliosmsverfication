2decef6bcf849346d3d1946b001175be3c4c4989
Chintan <munjanichintan@gmail.com>
2021-02-25 12:53:43 +0530

    Commit'

--- apiverification/utils.py
+++ apiverification/utils.py
@@ -2,14 +2,14 @@ import os
 from twilio.rest import Client
 
 
-account_sid = "AC4d012a1f4e6fe637206fcb3bcc4c43b6"
-auth_token = "3b580097ef1f3b4b42480aa3740af68e"
+account_sid = "AC****************************b6"
+auth_token = "3b******************************e"
 Client = Client(account_sid, auth_token)
 
 
 def send_sms(user_code, phone_number):
 	message = Client.messages.create(
 			body = f'Your verification code is: {user_code}',
-			from_ = "+16193893327",
+			from_ = "+1************",
 			to = f'{phone_number}'
 		) 
